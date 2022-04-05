57e702152e32e53a57e351606d8e7a4b65002ed7
sijunx <sijunx@163.com>
2022-04-05 20:37:23 +0800

    Update task_iflytek_adversarial_training.py

--- examples/task_iflytek_adversarial_training.py
+++ examples/task_iflytek_adversarial_training.py
@@ -15,15 +15,27 @@ from bert4keras.snippets import sequence_padding, DataGenerator
 from keras.layers import Lambda, Dense
 from tqdm import tqdm
 
-num_classes = 119
+from bert4keras.models import build_transformer_model, tf
+from tensorflow.python.framework.graph_util import convert_variables_to_constants
+
+from keras import backend as K
+from tensorflow.python.platform import gfile
+
+wkdir = '/Users/xusijun/Documents/NLP010/bert4keras-master_bert_060/examples'
+pb_filename = 'model_classify_1009000.pb'
+
+num_classes = 3
 maxlen = 128
 batch_size = 32
 
 # BERT base
-config_path = '/root/kg/bert/chinese_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = '/root/kg/bert/chinese_L-12_H-768_A-12/bert_model.ckpt'
-dict_path = '/root/kg/bert/chinese_L-12_H-768_A-12/vocab.txt'
+# config_path = '/root/kg/bert/chinese_L-12_H-768_A-12/bert_config.json'
+# checkpoint_path = '/root/kg/bert/chinese_L-12_H-768_A-12/bert_model.ckpt'
+# dict_path = '/root/kg/bert/chinese_L-12_H-768_A-12/vocab.txt'
 
+config_path = '/Users/xusijun/Documents/NLP009/bert4keras-master001/albert_tiny_google_zh_489k/albert_config.json'
+checkpoint_path = '/Users/xusijun/Documents/NLP009/bert4keras-master001/albert_tiny_google_zh_489k/albert_model.ckpt'
+dict_path = '/Users/xusijun/Documents/NLP009/bert4keras-master001/albert_tiny_google_zh_489k/vocab.txt'
 
 def load_data(filename):
     """加载数据
@@ -40,10 +52,12 @@ def load_data(filename):
 
 # 加载数据集
 train_data = load_data(
-    '/root/CLUE-master/baselines/CLUEdataset/iflytek/train.json'
+    # '/root/CLUE-master/baselines/CLUEdataset/iflytek/train.json'
+    '/Users/xusijun/Documents/NLP010/bert4keras-master_bert_060/examples/CLUEdataset/iflytek/train.json'
 )
 valid_data = load_data(
-    '/root/CLUE-master/baselines/CLUEdataset/iflytek/dev.json'
+    # '/root/CLUE-master/baselines/CLUEdataset/iflytek/dev.json'
+    '/Users/xusijun/Documents/NLP010/bert4keras-master_bert_060/examples/CLUEdataset/iflytek/dev.json'
 )
 
 # 建立分词器
@@ -66,7 +80,6 @@ class data_generator(DataGenerator):
                 batch_labels = sequence_padding(batch_labels)
                 yield [batch_token_ids, batch_segment_ids], batch_labels
                 batch_token_ids, batch_segment_ids, batch_labels = [], [], []
-
 
 # 转换数据集
 train_generator = data_generator(train_data, batch_size)
@@ -153,6 +166,74 @@ def evaluate(data):
     return right / total
 
 
+# save model to pb ====================
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = convert_variables_to_constants(session, input_graph_def,
+                                                      output_names, freeze_var_names)
+        return frozen_graph
+
+def my_keras_to_pb():
+    # save keras model as tf pb files ===============
+    frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name for out in model.outputs])
+    tf.train.write_graph(frozen_graph, wkdir, pb_filename, as_text=False)
+
+    # # load & inference the model ==================
+    with tf.Session() as sess:
+        # load model from pb file
+        with gfile.FastGFile(wkdir+'/'+pb_filename,'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            sess.graph.as_default()
+            g_in = tf.import_graph_def(graph_def)
+        # write to tensorboard (check tensorboard for each op names)
+        writer = tf.summary.FileWriter(wkdir+'/log/')
+        writer.add_graph(sess.graph)
+        writer.flush()
+        writer.close()
+        # print all operation names
+        # print('\n===== ouptut operation names =====\n')
+        # for op in sess.graph.get_operations():
+        #   print(op)
+
+def just_show():
+    s1 = u"亚洲金融危机，楼市泡沫，通货膨胀"
+    s2 = u"朝闻天下，足不出户知天下"
+    s3 = u"羽毛球比赛一直是热门比赛"
+    s4 = u"中国男篮世锦赛，小组取得第一名"
+    s5 = u"中国男足亚洲杯小组都没有出现"
+    s6 = u"经济如何脱实向虚"
+    s7 = u"实体经济将越来越被重视"
+    s8 = u"钢铁制造业是一个国家工业发展都基础"
+    s9 = u"废钢收购带动了一批人致富"
+
+    for s in [s1, s2, s3, s4, s5, s6, s7, s8, s9]:
+        token_ids, segment_ids = tokenizer.encode(s, maxlen=maxlen)
+        label = model.predict([[token_ids], [segment_ids]])[0].argmax()
+        print("预测对标签label", label)
+
 class Evaluator(keras.callbacks.Callback):
     """评估与保存
     """
@@ -163,11 +244,16 @@ class Evaluator(keras.callbacks.Callback):
         val_acc = evaluate(valid_generator)
         if val_acc > self.best_val_acc:
             self.best_val_acc = val_acc
-            model.save_weights('best_model.weights')
+            # model.save_weights('model_classify_06.weights')
+            model.save('./model_classify_107.h5')
         print(
             u'val_acc: %.5f, best_val_acc: %.5f\n' %
             (val_acc, self.best_val_acc)
         )
+        # 演示效果
+        just_show()
+        # 我的保存
+        my_keras_to_pb()
 
 
 def predict_to_file(in_file, out_file):
@@ -185,7 +271,6 @@ def predict_to_file(in_file, out_file):
             fw.write(l + '\n')
     fw.close()
 
-
 if __name__ == '__main__':
 
     evaluator = Evaluator()
@@ -193,7 +278,7 @@ if __name__ == '__main__':
     model.fit(
         train_generator.forfit(),
         steps_per_epoch=len(train_generator),
-        epochs=50,
+        epochs=2,
         callbacks=[evaluator]
     )
 
